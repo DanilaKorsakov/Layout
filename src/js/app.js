55ee4df4a60815414ca03b48f929619d858eb267
DannilaKorsakov <kvashnindan@gmail.com>
2022-08-13 19:28:14 +0300

    horizontal header animation

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,11 @@ let burger = document.getElementsByClassName('header__burger')[0];
 let cross = document.getElementsByClassName('header__close-btn')[0];
 let burger_menu = document.getElementsByClassName('header__mobile')[0];
 let logo = document.getElementsByClassName('header__top')[0].getElementsByClassName('header__logo')[0];
+let header = document.getElementsByClassName('header')[0];
+
+const screenWidth = document.documentElement.clientWidth;
+
+let header_hight = header.clientHeight;
 
 burger.addEventListener('click',burgerClick);
 
@@ -26,4 +31,29 @@ function crossClick() {
     burger_menu.classList.remove('active');
     logo.classList.remove('active');
 }
+
+if(screenWidth>1024) {
+
+    window.onscroll = changeHeader;
+
+    let lastScroll = 0;
+
+
+    function changeHeader() {
+
+        let top = document.documentElement.scrollTop;
+
+        if(lastScroll>top){
+            header.style.top = '0';
+        }else{
+            if(window.pageYOffset>200){
+                header.style.top=`-${header_hight}px`;
+            }
+        }
+
+        lastScroll = top;
+    }
+
+}
+
 
